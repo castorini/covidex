9575b8bc4fc3dbbce71d09b71544350b9a1514ea
Edwin Zhang <edwinzhang64@gmail.com>
2020-03-31 23:06:10 +0000

    Update deployment documentation for warm up

--- README.md
+++ README.md
@@ -10,12 +10,17 @@ To run the UI client, follow the instructions in [client/README.md](./client/REA
 
 #### Production Deployment
 
-Run the deployment script:
+Redirect port 80 to a specified port since only root can bind to port 80 (the below command uses port 8000):
+```
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
+```
+
+Run the deployment script to start the server and warm up models (deploys to port 8000 by default):
 ```
 sh deploy-prod.sh
 ```
 
-*Optional:* set the environment variable `$PORT` to change the server port (defaults to 8080):
+*Optional:* set the environment variable `$PORT`:
 ```
-PORT=8080 sh deploy-prod.sh
+PORT=8000 sh deploy-prod.sh
 ```
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # build client
+echo "Building client..."
 export BUILD_PATH=./client/build
 export STATIC_PATH=./api/app/static
 
@@ -14,4 +15,19 @@ mv $STATIC_PATH/static/* $STATIC_PATH
 cd api
 pip install -r requirements.txt
 export DEVELOPMENT=False
-uvicorn app.main:app --port=${PORT:-80} --host 0.0.0.0
+
+echo "Starting server..."
+PORT=${PORT:-8000}
+nohup uvicorn app.main:app --port=$PORT --host 0.0.0.0 &
+
+echo "Waiting for server availability..."
+l_TELNET=`echo "quit" | telnet localhost $PORT | grep "Escape character is"` > /dev/null
+while [ "$?" -ne 0 ]; do
+  sleep 10
+  l_TELNET=`echo "quit" | telnet localhost $PORT | grep "Escape character is"` > /dev/null
+done
+
+echo "Warming up models..."
+curl http://localhost:$PORT/api/search?query=test > /dev/null
+
+echo "Server started successfully!"
