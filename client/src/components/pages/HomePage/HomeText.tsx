8b3aa839cf28c661e0debabea3c4edb4235651be
Edwin Zhang <edwinzhang64@gmail.com>
2020-06-15 10:59:53 -0600

    CORD-19 06/12 update (#75)

--- client/src/components/pages/HomePage/HomeText.tsx
+++ client/src/components/pages/HomePage/HomeText.tsx
@@ -19,8 +19,8 @@ const HomeText = () => {
         <Link href="https://allenai.org/" target="_blank" rel="noopener noreferrer">
           Allen Institute for AI
         </Link>{' '}
-        (data release of May 26, 2020), which currently contains over 47,000 scholarly articles,
-        including over 36,000 with full text, about COVID-19 and coronavirus-related research, drawn
+        (data release of June 12, 2020), which currently contains over 140,000 scholarly articles,
+        including over 70,000 with full text, about COVID-19 and coronavirus-related research, drawn
         from a variety of sources including PubMed, a curated list of articles from the WHO, as well
         as preprints from bioRxiv and medRxiv. In addition, we also support search on 100+
         randomized controlled trials (published and ongoing) related to COVID-19 provided by{' '}
@@ -144,7 +144,11 @@ const HomeText = () => {
           Ralph Tang
         </Link>
         ,{' '}
-        <Link href="https://scholar.google.com/citations?user=xD32wZ8AAAAJ&hl=en" target="_blank" rel="noopener noreferrer">
+        <Link
+          href="https://scholar.google.com/citations?user=xD32wZ8AAAAJ&hl=en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Rodrigo Nogueira
         </Link>
         , and{' '}
